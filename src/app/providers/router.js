a3c2fb525990aac0dff72da17398a1a3aaf2f808
reqox <noreply@users.noreply.github.com>
2026-08-12 15:39:36 +0300

    правки роутинга hash -> web

--- src/app/providers/router.js
+++ src/app/providers/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 const routes = [
   {
@@ -19,7 +19,7 @@ const routes = [
 ];
 
 export const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes,
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) return savedPosition;
